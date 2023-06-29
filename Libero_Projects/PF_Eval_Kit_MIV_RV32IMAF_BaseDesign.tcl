d65c38c0237f4b12e66ec64f0327958465099763
Sebastian Slowikowski <Seb.Slowikowski@microchip.com>
2023-06-29 23:13:36 +0100

    Reverting back to old software program style

--- Libero_Projects/PF_Eval_Kit_MIV_RV32IMAF_BaseDesign.tcl
+++ Libero_Projects/PF_Eval_Kit_MIV_RV32IMAF_BaseDesign.tcl
@@ -78,8 +78,7 @@ if {[file exists $projectDir] == 1} then {
 
 # Copy the example software program into the project directory
 # file copy -force $scriptDir/import/software_example/$cpuGroup/$config/hex $projectDir
-puts "Info: File copy hex: $scriptDir/import/software_example/$cpuGroup/hex to: $projectDir"
-file copy -force $scriptDir/import/software_example/$cpuGroup/hex $projectDir
+file copy -force $scriptDir/import/software_example/$cpuGroup/$config/hex $projectDir
 
 # Import and build the design's SmartDesign
 print_message "Building the $sdName..."
--- Libero_Projects/PF_Eval_Kit_MIV_RV32_BaseDesign.tcl
+++ Libero_Projects/PF_Eval_Kit_MIV_RV32_BaseDesign.tcl
@@ -79,8 +79,7 @@ if {[file exists $projectDir] == 1} then {
 
 # Copy the example software program into the project directory (and bootloader elf for DGC1 and DGC2 configs)
 # file copy -force $scriptDir/import/software_example/$cpuGroup/$config/hex $projectDir
-puts "Info: File copy hex: $scriptDir/import/software_example/$cpuGroup/hex to: $projectDir"
-file copy -force $scriptDir/import/software_example/$cpuGroup/hex $projectDir
+file copy -force $scriptDir/import/software_example/$softCpu/$config/hex $projectDir
 if {$config in {"DGC1" "DGC2"}} {file copy -force $scriptDir/import/software_example/$softCpu/$config/bootloader_elf $projectDir}
 
 # Import and build the design's SmartDesign
